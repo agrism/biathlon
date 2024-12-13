a469c99cf24bbad077c756a1f7bb4f1c74212eeb
Agris <7924@inbox.lv>
2024-12-13 21:41:03 +0200

    add scheduler

--- config/schedule.php
+++ config/schedule.php
@@ -1,8 +1,14 @@
 <?php
 
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Log;
 
 return function (Schedule $schedule) {
+
+    $schedule->call(function() {
+        Log::info('Scheduler test: ' . now());
+    })->everyMinute();
+
     $schedule->command('app:read-competition-results')->everyMinute();
     $schedule->command('app:read-forecast-results-command')->everyMinute();
 };
