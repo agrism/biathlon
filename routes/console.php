a9d9a1a3b3cd73f3e3494536ab9bea21469586d5
Agris <7924@inbox.lv>
2024-12-13 22:34:50 +0200

    add scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote1')->hourly();
+})->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('app:read-competition-results')->everyMinute();
+Schedule::command('app:read-forecast-results-command')->everyMinute();
