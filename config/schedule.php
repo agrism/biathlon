f09be25d2330037fd11b77d8d6c931fb97d14a74
Agris <7924@inbox.lv>
2024-12-13 21:33:51 +0200

    add scheduler

--- config/schedule.php
+++ config/schedule.php
@@ -3,6 +3,6 @@
 use Illuminate\Console\Scheduling\Schedule;
 
 return function (Schedule $schedule) {
-    $schedule->command('app:read-competition-results')->everyFiveMinutes();
+    $schedule->command('app:read-competition-results')->everyMinute();
     $schedule->command('app:read-forecast-results-command')->everyMinute();
 };
