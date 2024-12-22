bc6fa9886c18a10b9c2558ead2ffb5256152591d
Agris <7924@inbox.lv>
2024-12-22 12:16:08 +0200

    add tooltip for stat

--- resources/views/components/cards/athlete-stat.blade.php
+++ resources/views/components/cards/athlete-stat.blade.php
@@ -10,9 +10,9 @@
             {{$athlete->stats?->statShootingProne === null ? '-' : $athlete->stats->statShootingProne.'%' }})
             {{$athlete->stats?->statShootingStanding === null ? '-' : $athlete->stats->statShootingStanding.'%' }}
         </span>
-        <span class="absolute invisible group-hover:visible opacity-0 group-hover:opacity-100 transition bg-gray-800 text-white text-sm rounded py-1 px-2 -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap">
-        (skiing, shooting prone, shooting standing)
-    </span>
+{{--        <span class="absolute invisible group-hover:visible opacity-0 group-hover:opacity-100 transition bg-gray-800 text-white text-sm rounded py-1 px-2 -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap">--}}
+{{--        (skiing, shooting prone, shooting standing)--}}
+{{--    </span>--}}
     </div>
 </small>
 @endif
