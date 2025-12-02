73891ccb939c9d736e3c027147ddbfd1eea529e3
Agris <7924@inbox.lv>
2025-12-02 15:46:02 +0200

    show athlete details

--- app/Http/Controllers/Athletes/ShowController.php
+++ app/Http/Controllers/Athletes/ShowController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Athletes;
 
+use App\Helpers\LinkHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Athlete;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -11,10 +13,16 @@ class ShowController extends Controller
 {
     public function __invoke(Request $request, string $id): View
     {
-        $athlete = Athlete::query()->where('ibu_id', $id)->first();
+        $athlete = Athlete::query()->with([
+            'results' => function (Relation $query) {
+                return $query->orderByDesc('start_time')->with('competition');
+            }
+        ])->where('ibu_id', $id)->first();
 
-        $this->registerBread('Athlete:'. $athlete->family_name);
+        $this->registerBread('Athlete:'.$athlete->family_name);
 
-        return view('athletes.show', compact('athlete'));
+        $linkHelper = app(LinkHelper::class);
+
+        return view('athletes.show', compact('athlete', 'linkHelper'));
     }
 }
--- resources/views/athletes/show.blade.php
+++ resources/views/athletes/show.blade.php
@@ -83,6 +83,59 @@
             </table>
         </div>
 
+        <h1 class="mb-4 mt-4 text-1xl font-extrabold leading-none tracking-tight text-gray-900 md:text-1xl lg:text-2xl  text-center">Results</h1>
+
+        <div class="relative flex flex-col w-full h-full overflow-scroll text-gray-700">
+            <table class="w-full text-left table-auto min-w-max">
+                <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>Competition</th>
+                    <th>Shooting</th>
+                    <th>Behind</th>
+                </tr>
+                </thead>
+                <tbody>
+                @php /** @var $result \App\Models\EventCompetitionResult */ @endphp
+                @php /** @var $linkHelper \App\Helpers\LinkHelper */ @endphp
+                @foreach($athlete->results as $i => $result)
+
+                <tr class="hover:bg-slate-50">
+                    <td class="p-4 border-b border-slate-200">
+                        <p class="block text-sm text-slate-800">
+                            @if($result->rank == 1) {{\App\Enums\RankEnum::PLACE1->getMedal()}}
+                            @elseif($result->rank == 2) {{\App\Enums\RankEnum::PLACE2->getMedal()}}
+                            @elseif($result->rank == 3) {{\App\Enums\RankEnum::PLACE3->getMedal()}}
+                            @elseif($result->rank)
+                                {!! $linkHelper->getLink(route: route('competitions.show', $result->competition->race_remote_id),name: $result->rank) !!}
+                            @else
+                              DNF
+                            @endif
+                        </p>
+                    </td>
+                    <td class="p-4 border-b border-slate-200">
+                        <p class="block text-sm text-slate-800">
+                            {!! $linkHelper->getLink(route: route('competitions.show', $result->competition->race_remote_id),name: $result->competition->getTitle()) !!}
+                        </p>
+                    </td>
+                    <td class="p-4 border-b border-slate-200">
+                        <p class="block text-sm text-slate-800">
+                            {!! $linkHelper->getLink(route: route('competitions.show', $result->competition->race_remote_id),name: $result->shootings) !!}
+
+                        </p>
+                    </td>
+                    <td class="p-4 border-b border-slate-200">
+                        <p class="block text-sm text-slate-800">
+                            {!! $linkHelper->getLink(route: route('competitions.show', $result->competition->race_remote_id),name: $result->behind) !!}
+                        </p>
+                    </td>
+                </tr>
+
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+
 
     </div>
 
