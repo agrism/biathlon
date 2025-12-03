07573bcde9cec9adf58078324cdb1b5b2c3b16a5
Agris <7924@inbox.lv>
2025-12-03 13:06:11 +0200

    upd test route

--- routes/web.php
+++ routes/web.php
@@ -156,7 +156,7 @@ Route::get('/test-dainis', function (Request $request){
                 'name' => $user->name,
                 'points' => [
                     [
-                        'type' => $forecast->type->name,
+                        'type' => \App\Enums\Forecast\ForecastTypeEnum::FORECAST_FIRST_SIX_PLACES->name,
                         'points' => [
                             'regular' => $oldRegular,
                             'bonus' => $oldBonus,
@@ -337,6 +337,25 @@ HTML;
         echo '</table>';
     }
     echo '<hr>';
+
+
+    echo '<table>';
+    echo '<tr>';
+    foreach (['Agris', 'Grey', 'Dainis', 'Andris'] as $name){
+        echo '<th colspan="3">'.$name.'</th>';
+    }
+    echo '</tr>';
+    echo '<tbody>';
+    foreach ($return as $forecast){
+        echo '<tr>';
+        echo '<td colspan="100"><a href="'.(data_get($forecast, 'url')).'" target="_blank">'.data_get($forecast, 'name').'</a></td>';
+        foreach ($forecast['users'] as $user){
+            echo '<td> ' . ($user['points'][0]['points']['total'] ?? 0). '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '<tbody>';
+    echo '</table>';
 
     echo '<pre>';
     echo json_encode($return, JSON_PRETTY_PRINT);
