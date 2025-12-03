3aa2d131e66bc1f4e6ed9340356f95297375abd2
Agris <7924@inbox.lv>
2025-12-03 13:23:18 +0200

    upd test route

--- routes/web.php
+++ routes/web.php
@@ -358,7 +358,7 @@ HTML;
         echo '<tr>';
         echo '<td colspan="1"><a href="'.(data_get($forecast, 'url')).'" target="_blank">'.data_get($forecast, 'name').'</a></td>';
         foreach ($forecast['users'] ?? [] as $user){
-            echo '<td data="'. data_get($user, 'name').'"> ' . $total[data_get($user, 'name')] += ($user['points'][0]['points']['total'] ?? 0). '</td>';
+            echo '<td data="'. data_get($user, 'name').'"> ' . $total[data_get($user, 'name')] += intval($user['points'][0]['points']['total'] ?? 0). '</td>';
         }
         echo '</tr>';
     }
